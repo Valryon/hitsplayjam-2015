201f1edcdf4db95391d9268350cba8f6fdf9ce64
Damien Mayance <dmayance@gmail.com>
2015-07-11 00:14:41 +0200

    Owoui

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 5f;
+	public float shootForce = 1f;
+	public float energy = 1f;
+
+	private Vector2 movement;
+	private Rigidbody rbody;
+
+	void Awake()
+	{
+		rbody = GetComponent<Rigidbody> ();
+	}
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+    Debug.Log (z);
+
+		movement = new Vector3 (x, 0, z) * speed;
+	}
+
+	void FixedUpdate()
+	{
+		rbody.velocity = movement;
+	}
+}
