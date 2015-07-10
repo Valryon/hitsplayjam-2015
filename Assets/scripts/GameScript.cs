2b3f5cace4814ed4af4a49eee00a32fa2ec0240a
Damien Mayance <dmayance@gmail.com>
2015-07-11 01:08:39 +0200

    Player swap

--- Assets/scripts/GameScript.cs
+++ Assets/scripts/GameScript.cs
@@ -12,16 +12,19 @@ public class GameScript : MonoBehaviour
   public List<PlayerScript> team2;
 
   private PlayerScript player1, player2;
+  private int player1Index, player2Index;
 
   void Awake()
   {
     var players = FindObjectsOfType<PlayerScript> ();
 
+    player1Index = 0;
     team1 = players.Where (p => p.team == TEAM1).ToList ();
-    SelectTeam(team1[0], TEAM1);
+    SelectTeam(team1[player1Index], TEAM1);
 
+    player2Index = 0;
     team2 = players.Where (p => p.team == TEAM2).ToList ();
-    SelectTeam(team2[0], TEAM2);
+    SelectTeam(team2[player2Index], TEAM2);
   }
 
   void Start () 
@@ -31,7 +34,25 @@ public class GameScript : MonoBehaviour
 	
 	void Update () 
   {
-	
+	  // Team: change selected character
+    if (Input.GetKeyDown(KeyCode.RightShift)) 
+    {
+      player1Index++;
+      if(player1Index >= team1.Count)
+      {
+        player1Index = 0;
+      }
+      SelectTeam(team1[player1Index], TEAM1);
+    }
+    if (Input.GetKeyDown(KeyCode.E)) 
+    {
+      player2Index++;
+      if(player2Index >= team2.Count)
+      {
+        player2Index = 0;
+      }
+      SelectTeam(team2[player2Index], TEAM2);
+    }
 	}
 
   private void SelectTeam(PlayerScript p, int team)
