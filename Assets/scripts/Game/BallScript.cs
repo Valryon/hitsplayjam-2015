97702834d8cae9cfb19d99192be418b3afaf9ef9
Damien Mayance <dmayance@gmail.com>
2015-07-11 12:24:37 +0200

    Ball: stick to player WIP

--- Assets/scripts/Game/BallScript.cs
+++ Assets/scripts/Game/BallScript.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class BallScript : MonoBehaviour 
 {
   public int lastTeamTouch;
+  public PlayerScript linkedPlayer;
 
   private Rigidbody rbody;
   private Vector3 startPosition;
 	
-
   void Awake()
   {
     rbody = GetComponent<Rigidbody> ();
 
     startPosition = this.transform.position;
+  }
+
+  void Update()
+  {
+    if (linkedPlayer != null) 
+    {
+      this.transform.position = linkedPlayer.ballPosition.position;
+    }
   }
 
   public void Reset()
@@ -25,13 +33,19 @@ public class BallScript : MonoBehaviour
   }
 
 
-  void OnCollisionEnter(Collision other){
+  void OnCollisionEnter(Collision other)
+  {
     var col = other.collider;
     if (col.tag != "Player")
       return;
     var p = col.GetComponent<PlayerScript> ();
     lastTeamTouch = p.team;
     Debug.Log ("poc by ");
+  }
 
+  public bool IsPickable 
+  {
+    get;
+    set;
   }
 }
--- Assets/scripts/Game/PlayerScript.cs
+++ Assets/scripts/Game/PlayerScript.cs
@@ -6,11 +6,13 @@ public class PlayerScript : MonoBehaviour
   public int team = 1;
   public int number = 1;
   public PlayerDefinition definition;
+  public Transform ballPosition;
 
 	private Vector3 movement;
 	private Rigidbody rbody;
   private Renderer render;
 
+  private BallScript ball;
   private GameScript gameScript;
 
 	void Awake()
@@ -38,51 +40,62 @@ public class PlayerScript : MonoBehaviour
 	{
     movement = Vector3.zero;
 
-    if (IsSelected) 
-    {
+    if (IsSelected) {
       float x = 0f;
       float z = 0f;
 
       // Team 1: arrows
-      if(team == GameScript.TEAM1)
-      {
+      if (team == GameScript.TEAM1) {
         x = Input.GetAxis ("Horizontal");
-        if( x < -0.25f) x = -1f;
-        else if( x > 0.25f) x = 1f;
+        if (x < -0.25f)
+          x = -1f;
+        else if (x > 0.25f)
+          x = 1f;
 
         z = Input.GetAxis ("Vertical");
-        if( z < -0.25f) z = -1f;
-        else if( z > 0.25f) z = 1f;
+        if (z < -0.25f)
+          z = -1f;
+        else if (z > 0.25f)
+          z = 1f;
       }
       // Team 2: ZQSD
-      else if(team == GameScript.TEAM2)
-      {
-        if(Input.GetKey(KeyCode.Q))
-        {
+      else if (team == GameScript.TEAM2) {
+        if (Input.GetKey (KeyCode.Q)) {
           x = -1f; 
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
+        } else if (Input.GetKey (KeyCode.D)) {
           x = 1f;
         }
-        if(Input.GetKey(KeyCode.Z))
-        {
+        if (Input.GetKey (KeyCode.Z)) {
           z = 1f; 
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
+        } else if (Input.GetKey (KeyCode.S)) {
           z = -1f;
         }
       }
 
       movement = new Vector3 (x, 0, z) * definition.speed;
     }
-	}
+  }
 
 	void FixedUpdate()
 	{
 		rbody.velocity = movement;
 	}
+
+  void OnTriggerEnter(Collider c)
+  {
+    BallScript b = c.GetComponent<BallScript> ();
+
+    // Touching the ball
+    if (b != null) 
+    {
+      // Link?
+      if(b.linkedPlayer == null && b.IsPickable)
+      {
+        b.linkedPlayer = this;
+        this.ball = b;
+      }
+    }
+  }
 
   public bool IsSelected
   {
