71174e85927225bf18a03a48ddb17020c77962a7
Damien Mayance <dmayance@gmail.com>
2015-07-12 01:00:26 +0200

    Titlescreen WIP

---
+++ Assets/scripts/Menu/SelectionScreenItemScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SelectionScreenItemScript : MonoBehaviour 
+{
+  public PlayerDefinition defition;
+  public Image image;
+  public Text text;
+
+  void Awake()
+  {
+    if (defition != null) {
+      image.sprite = defition.avatar;
+      text.text = defition.shiityName;
+    }
+  }
+}
--- Assets/scripts/Menu/TitleScreenScript.cs
+++ Assets/scripts/Menu/TitleScreenScript.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class TitleScreenScript : MonoBehaviour 
 {
+  void Start()
+  {
+    SoundsScript.Play ("credits", this.transform.position);
+  }
+
 	public void StartSelection()
   {
     Application.LoadLevel ("Selection");
