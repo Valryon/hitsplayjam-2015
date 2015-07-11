59b03e3779ca2857cd884e367c7b89aba8226c55
Damien Mayance <dmayance@gmail.com>
2015-07-11 14:47:12 +0200

    Shitty billboard

--- Assets/scripts/Components/BillboardScript.cs
+++ Assets/scripts/Components/BillboardScript.cs
@@ -7,6 +7,6 @@ public class BillboardScript : MonoBehaviour
   {
     float angle = Camera.main.transform.rotation.eulerAngles.y;
 
-    //this.transform.rotation = Quaternion.Euler (this.transform.rotation.eulerAngles.x, angle, this.transform.rotation.eulerAngles.z);
+    this.transform.rotation = Quaternion.Euler (this.transform.rotation.eulerAngles.x, angle, this.transform.rotation.eulerAngles.z);
 	}
 }
