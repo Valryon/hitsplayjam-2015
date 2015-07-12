a06e189af29c946209b193c451a7f76a27bd8571
Damien Mayance <dmayance@gmail.com>
2015-07-12 14:43:50 +0200

    Sons

--- Assets/scripts/Menu/TitleScreenScript.cs
+++ Assets/scripts/Menu/TitleScreenScript.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TitleScreenScript : MonoBehaviour 
 {
+  public AudioSource music;
   public SimpleAnimatorUI animator;
+  public Button button;
 
   void Awake()
   {
+    music.enabled = false;
     animator.Play("start");
   }
 
   void Start()
   {
+    SoundsScript.Play ("title", Vector3.zero);
+
+    StartCoroutine (Timer.Start (1f, () => {
+      music.enabled = true;
+    }));
   }
 
 	public void StartSelection()
   {
-    Application.LoadLevel ("Selection");
+    SoundsScript.Play ("play", Vector3.zero);
+
+    button.interactable = false;
+
+    StartCoroutine (Timer.Start (0.35f, () => {
+      Application.LoadLevel ("Selection");
+    }));
   }
 }
